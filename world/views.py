7fcf3b6d9dda88cf97896e866ec4640ec52c9ac2
thomas-95is <thomas.stokes@informed.com>
2018-12-02 10:53:53 +0000

    views.py: Add view for UK border.

--- world/views.py
+++ world/views.py
@@ -1,3 +1,9 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
+from .models import WorldBorder
 
-# Create your views here.
+
+def geojson(request):
+    layer = WorldBorder.objects.filter(name='United Kingdom')
+    geojson = serialize('geojson', layer)
+    return render(request, 'world/map.html', {'my_geojson': geojson})
